2f33717560a8dbceafb50b21eda717e49efaa8ee
GasanaJr <gasanadj2003@gmail.com>
2024-06-30 00:07:56 +0200

    feat(login): make login responsive

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -28,46 +29,50 @@ class AuthScreen extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 11, 28, 37),
         ),
       ),
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color(0xFF143342),
-            Colors.white,
-          ], begin: Alignment.topRight, end: Alignment.bottomRight)),
-          child: Column(
-            children: [
-              Expanded(
-                child: Column(
-                  children: [
-                    Center(
-                      child: Image.asset(
-                        'assets/logo.png',
-                        width: 200,
-                        height: 150,
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        "FIP",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+      body: ListView(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF143342),
+                  Colors.white,
+                ],
+                begin: Alignment.topRight,
+                end: Alignment.bottomRight,
               ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  decoration: BoxDecoration(
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, bottom: 20.0),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/logo.png',
+                      width: 200,
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "FIP",
+                    style: TextStyle(
                       color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30))),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 20, vertical: 30),
@@ -88,14 +93,14 @@ class AuthScreen extends StatelessWidget {
                           padding: const EdgeInsets.only(bottom: 15.0),
                           child: TextField(
                             decoration: InputDecoration(
-                                labelText: "Enter your Email",
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10))),
+                              labelText: "Enter your Email",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
                           ),
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
+                        SizedBox(height: 5),
                         Padding(
                           padding: const EdgeInsets.only(bottom: 15),
                           child: Text(
@@ -108,76 +113,72 @@ class AuthScreen extends StatelessWidget {
                         ),
                         TextField(
                           decoration: InputDecoration(
-                              labelText: "Password",
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10))),
+                            labelText: "Password",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
                           obscureText: true,
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
+                        SizedBox(height: 20),
                         Center(
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 20.0, bottom: 20),
-                                child: ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.pushReplacementNamed(context, '/home');
-                                    },
-                                    style: ButtonStyle(
-                                        backgroundColor:
-                                            MaterialStatePropertyAll<Color>(
-                                                Color(0xFF143342)),
-                                        foregroundColor:
-                                            MaterialStatePropertyAll<Color>(
-                                                Colors.white),
-                                        textStyle:
-                                            MaterialStatePropertyAll<TextStyle>(
-                                                TextStyle(
-                                                    fontSize: 20,
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                        minimumSize:
-                                            MaterialStatePropertyAll<Size>(
-                                                Size(150, 50))),
-                                    child: Text("Log In")),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, '/home');
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Color(0xFF143342)),
+                              foregroundColor:
+                                  MaterialStatePropertyAll<Color>(Colors.white),
+                              textStyle: MaterialStatePropertyAll<TextStyle>(
+                                TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                              RichText(
-                                  text: TextSpan(
-                                      text: "Forgot your Password? ",
-                                      style: TextStyle(
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black),
-                                      children: <TextSpan>[
-                                    TextSpan(
-                                        text: 'Click here',
-                                        style: TextStyle(
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.blue,
-                                          decoration: TextDecoration.underline,
-                                        ),
-                                        recognizer: TapGestureRecognizer()
-                                          ..onTap = () {
-                                            // Navigator.of(context).pushReplacementNamed('/report');
-                                            Navigator.pushNamed(
-                                                context, '/report');
-                                          })
-                                  ]))
-                            ],
+                              minimumSize:
+                                  MaterialStatePropertyAll<Size>(Size(150, 50)),
+                            ),
+                            child: Text("Log In"),
                           ),
-                        )
+                        ),
+                        SizedBox(height: 20),
+                        Center(
+                          child: RichText(
+                            text: TextSpan(
+                              text: "Forgot your Password? ",
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Click here',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Navigator.pushNamed(context, '/report');
+                                    },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }

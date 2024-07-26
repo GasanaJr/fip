62dbba1929ce0544e04d984ec5c74b6756524f9d
DimtiriKwihangana <kwihanganadimtiri@gmail.com>
2024-07-26 17:26:21 +0200

    dashboard componet

--- lib/components/DashboardComponent.dart
+++ lib/components/DashboardComponent.dart
@@ -2,12 +2,7 @@ class DashboardComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Dashboard')),
-      body: Column(
-        children: [
-          ChartWidget(),
-          SummaryCard(),
-          ReportListComponent(),
+      
         ],
       ),
     );

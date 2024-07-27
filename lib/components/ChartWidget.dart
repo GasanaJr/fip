38c9845b223fe4945962011ad40dbda4bb1dba58
nalule-grace <g.nalule@alustudent.com>
2024-07-27 14:55:54 +0200

    linechart

--- lib/components/ChartWidget.dart
+++ lib/components/ChartWidget.dart
@@ -1,14 +1,18 @@
 class ChartWidget extends StatelessWidget {
   final List<ChartData> data;
   
+  //Constructor for chartwidget, requiring a list of chartdata
   ChartWidget({required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      //set the height of container to 300 pixels
       height: 300,
+      //create a linechart using the chart_flutter package
       child: charts.LineChart(
         [
+          //define a series of data for the chart
           charts.Series<ChartData, String>(
             id: 'Data',
             domainFn: (data, _) => data.label,
